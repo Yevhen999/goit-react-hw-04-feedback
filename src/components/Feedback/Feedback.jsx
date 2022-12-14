899ce21ef3e35d6737f76bd0056a2f15f60c00ce
Yevhen <leshchenkoyevhen@gmail.com>
2022-12-13 19:30:58 -0500

    change class to hook in feedback

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,58 +1,55 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import css from './Feedback.module.css';
 import Statistics from './Statistics/Statistics';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Section from './Section/Section';
 import Notification from './Notification/Notification';
 
-class Feedback extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+export const Feedback = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const options = ['good', 'neutral', 'bad'];
+
+  const total = good + neutral + bad;
+  const percentage = ((good / total) * 100).toFixed(0);
+
+  const handleIncrementFeedback = option => {
+    switch (option) {
+      case 'good':
+        setGood(good + 1);
+        break;
+      case 'neutral':
+        setNeutral(neutral + 1);
+        break;
+      case 'bad':
+        setBad(bad + 1);
+        break;
+      default:
+        console.log('choose the option');
+        break;
+    }
   };
 
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
-    const total = good + neutral + bad;
-    return total;
-  };
-
-  countPositiveFeedbackPercentage = () => {
-    return ((this.state.good / this.countTotalFeedback()) * 100).toFixed(0);
-  };
-
-  handleIncrementFeedback = option => {
-    this.setState(curState => ({
-      [option]: curState[option] + 1,
-    }));
-  };
-
-  render() {
-    const { good, neutral, bad } = this.state;
-    const total = this.countTotalFeedback();
-    const percentage = this.countPositiveFeedbackPercentage();
-    return (
-      <div className={css.feedbackWrapper}>
-        <Section title={'Please leave feedback'}>
-          <FeedbackOptions
-            options={Object.keys(this.state)}
-            onLeaveFeedback={this.handleIncrementFeedback}
-          />
-        </Section>
-        <Section title={'Statistics'}>
-          <Statistics
-            good={good}
-            neutral={neutral}
-            bad={bad}
-            total={total}
-            percentage={percentage}
-          />
-          {total === 0 && <Notification message={'There is no feedback'} />}
-        </Section>
-      </div>
-    );
-  }
-}
-
-export default Feedback;
+  return (
+    <div className={css.feedbackWrapper}>
+      <Section title={'Please leave feedback'}>
+        <FeedbackOptions
+          options={options}
+          onLeaveFeedback={handleIncrementFeedback}
+        />
+      </Section>
+      <Section title={'Statistics'}>
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={total}
+          percentage={percentage}
+        />
+        {total === 0 && <Notification message={'There is no feedback'} />}
+      </Section>
+    </div>
+  );
+};
